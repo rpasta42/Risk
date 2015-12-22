1b2109444e29f0ee959d034ee245a2fa0e8eb75e
Kostyantyn Kovalskyy <kostelkow@gmail.com>
2015-12-21 20:40:02 -0500

    Replaced 4-space tabs with 3.

--- GameBoard.py
+++ GameBoard.py
@@ -2,67 +2,66 @@
 #TODO: removeme from pycloak.collections import sub_lst
 
 class Country:
-    def __init__(self, name, continent):
-        self.name = name
-        self.team = -1 #-1 = none, then 0-infinity
-        self.continent = continent
-        self.num_troops = 0
+   def __init__(self, name, continent):
+      self.name = name
+      self.team = -1 #-1 = none, then 0-infinity
+      self.continent = continent
+      self.num_troops = 0
 
-    def add_troops(self, n): #TODO: check
-        self.num_troops += n
-    def change_team(self, team): #TODO check
-        self.team = team
-
+   def add_troops(self, n): #TODO: check
+      self.num_troops += n
+   def change_team(self, team): #TODO check
+      self.team = team
 
 class GameBoard:
-    def __init__(self, countries, continents):
-        self.neighbor_info = countries
-        self.continents = continents
-        self._run_map_tests()
+   def __init__(self, countries, continents):
+      self.neighbor_info = countries
+      self.continents = continents
+      self._run_map_tests()
 
-        self.countries = {}
-        for country in self.neighbor_info:
-            self.contries[country] = Country(country, contitents[country])
-            #self.troops[country] = 0
+      self.countries = {}
+      for country in self.neighbor_info:
+         self.contries[country] = Country(country, contitents[country])
+         #self.troops[country] = 0
 
-    def touching(self, c1, c2):
-        self.check_exists(c1, c2)
+   def touching(self, c1, c2):
+      self.check_exists(c1, c2)
 
-    def check_exists(self, *countries):
-        for country in countries:
-            if self.neighbor_info.get(country, None) is None:
-                raise Exception("Country doesn't exist: %s" % country)
-                return False
-        return True
+   def check_exists(self, *countries):
+      for country in countries:
+         if self.neighbor_info.get(country, None) is None:
+            raise Exception("Country doesn't exist: %s" % country)
+            return False
+      return True
 
-    def _run_map_tests(self):
-        self._check_self_neighbor(self.neighbor_info)
-        self._check_neighbors_exist(self.neighbor_info)
-        self._check_duplex(self.neighbor_info)
-        self._check_pathways(self.neighbor_info)
-        self._check_continents()
+   def _run_map_tests(self):
+      self._check_self_neighbor(self.neighbor_info)
+      self._check_neighbors_exist(self.neighbor_info)
+      self._check_duplex(self.neighbor_info)
+      self._check_pathways(self.neighbor_info)
+      self._check_continents()
 
-    def _check_continents(self):
-        #TODO: implement continent checks, maybe move it to same structure as countries
-        #make sure all countries in continent touch at least 1 other country in continent
-        #make sure every country is part of continent
-        #make sure they're not part of 2 different continents
-        #make sure there isn't continents without countries
-        pass
+   def _check_continents(self):
+      #TODO: implement continent checks, maybe move it to same structure as countries
+      #make sure all countries in continent touch at least 1 other country in continent
+      #make sure every country is part of continent
+      #make sure they're not part of 2 different continents
+      #make sure there isn't continents without countries
+      pass
 
-    def _check_neighbors_exist(self, countries):
-        for country in countries:
-            neighbors = countries.get(country, None)
-            if neighbors is None or len(neighbors) == 0:
-                raise Exception('Country cannot have empty neighbors')
-            self.check_exists(*neighbors)
+   def _check_neighbors_exist(self, countries):
+      for country in countries:
+         neighbors = countries.get(country, None)
+         if neighbors is None or len(neighbors) == 0:
+            raise Exception('Country cannot have empty neighbors')
+         self.check_exists(*neighbors)
 
-    def _check_self_neighbor(self, countries):
-        for country in countries:
-            neighbors = countries[country]
-            for neighbor in neighbors:
-                if country == neighbor:
-                    raise Exception("Country '%s' cannot be it's own neighbor" % country)
+   def _check_self_neighbor(self, countries):
+      for country in countries:
+         neighbors = countries[country]
+         for neighbor in neighbors:
+            if country == neighbor:
+               raise Exception("Country '%s' cannot be it's own neighbor" % country)
 
     def _check_pathways(self, countries):
         for start in countries:
@@ -70,27 +69,27 @@ class GameBoard:
                 if not self._connects(start, end, []):
                     raise Exception('Countries not connected: %s %s' % (start, end))
 
-    def _connects(self, start, end, checked=[]):
-        if start == end:
-            return True
-        if self.touching(start, end):
-            return True
-        else:
-            end_neighbors = self.neighbor_info[end]
-            for neighbor in end_neighbors:
-                if self.touching(start, neighbor):
-                    return True
-                if not (neighbor in checked):
-                    checked.append(neighbor)
-                    if self._connects(start, neighbor, checked):
-                        return True
-        return False
+   def _connects(self, start, end, checked=[]):
+      if start == end:
+         return True
+      if self.touching(start, end):
+         return True
+      else:
+         end_neighbors = self.neighbor_info[end]
+         for neighbor in end_neighbors:
+            if self.touching(start, neighbor):
+               return True
+            if not (neighbor in checked):
+               checked.append(neighbor)
+               if self._connects(start, neighbor, checked):
+                  return True
+      return False
 
-    def _check_duplex(self, countries):
-        for name in countries:
-            neighbors = countries[name]
-            for neighbor in neighbors:
-                if not name in countries[neighbor]:
-                    msg = 'Non-duplex: %s has %s, but not vice-versa' % (name, neighbor)
-                    raise Exception(msg)
+   def _check_duplex(self, countries):
+      for name in countries:
+         neighbors = countries[name]
+         for neighbor in neighbors:
+            if not name in countries[neighbor]:
+               msg = 'Non-duplex: %s has %s, but not vice-versa' % (name, neighbor)
+               raise Exception(msg)
 
--- main.py
+++ main.py
@@ -6,53 +6,59 @@ from GameBoard import GameBoard #, Country
 #but GameBoard and Player are both part of Game
 #Should we use inheritance or pass callbacks, etc?
 class Player:
-    def __init__(self):
-        pass
+   def __init__(self):
+      pass
 
-    #pre-game things
-    def chooseCountry(self, allCountries, freeCountries):
-        pass
-    #returns list of where it added people e.g. {'China': 1, 'US': 2}
-    def reinforce(self, ownCountries):
-        #self.chooseCountry = chooseCountryCallback
-    def move(self, start, finish):
-		pass
+   def setGameBoard(self, gameBoard):
+
+   #pre-game things
+   #TODO: AI needs more info to analyze things
+   def chooseCountry(self, freeCountries):
+     return freeCountries[0]
+
+   #returns list of where it added people e.g. {'China': 1, 'US': 2}
+   def reinforce(self, numTroops, ownCountries):
+      #self.chooseCountry = chooseCountryCallback
+
+   #{'start':'China', 'end':'Russia', 'count':10}
+   def move(self):
+     pass
 
 #choice or automatic selection
 class Game:
-    def __init__(self, players, gameBoard, chooseAuto=False):
-        self.gameBoard = gameBoard
-        self.players = players
-        self.chooseAuto = chooseAuto
+   def __init__(self, players, gameBoard, chooseAuto=False):
+      self.gameBoard = gameBoard
+      self.players = players
+      self.chooseAuto = chooseAuto
 
-    def begin(self):
-        pass
+   def begin(self):
+      pass
 
 def main():
-    countries = {
-        'Ukraine'   : ['Russia', 'France'],
-        'Russia'    : ['China', 'Ukraine', 'Canada', 'Sweden'],
-        'US'        : ['Canada'],
-        'Canada'    : ['US', 'Russia'],
-        'China'     : ['Russia'],
-        'Britain'   : ['France', 'Sweden'],
-        'Sweden'    : ['Russia', 'Britain'],
-        'France'    : ['Ukraine', 'Britain', 'Italy'],
-        'Italy'     : ['France']
-    }
+   countries = {
+      'Ukraine'   : ['Russia', 'France'],
+      'Russia'    : ['China', 'Ukraine', 'Canada', 'Sweden'],
+      'US'        : ['Canada'],
+      'Canada'    : ['US', 'Russia'],
+      'China'     : ['Russia'],
+      'Britain'   : ['France', 'Sweden'],
+      'Sweden'    : ['Russia', 'Britain'],
+      'France'    : ['Ukraine', 'Britain', 'Italy'],
+      'Italy'     : ['France']
+   }
 
-    continents = {
-        'Europe' : ['Ukraine', 'Britain', 'Sweden', 'France', 'Italy'],
-        'Asia'   : ['Russia', 'China'],
-        'Merica' : ['US', 'Canada']
-    }
+   continents = {
+      'Europe' : ['Ukraine', 'Britain', 'Sweden', 'France', 'Italy'],
+      'Asia'   : ['Russia', 'China'],
+      'Merica' : ['US', 'Canada']
+   }
 
-    gameBoard = GameBoard(countries, continents)
-    players = [Player(), Player()]
+   gameBoard = GameBoard(countries, continents)
+   players = [Player(), Player()]
 
-    game = Game(players, gameBoard)
+   game = Game(players, gameBoard)
 
-    return m
+   return game
 
 #importlib.reload(main)
 a = main()
